3f34ad81d3c303bba712c3e5d966da86fee0e28a
DN2 <david@sleepers.pro>
2018-11-23 14:02:13 -0800

    Add error checks and logging.

--- broadlink.indigoPlugin/Contents/Server Plugin/plugin.py
+++ broadlink.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -9,8 +9,9 @@ import time
 import indigo
 from broadlink import broadlink
 
-# Magic broadlink device IDs
+# This dict constant is used to map IDs to a human name.
 MODELS = {
+    # Magic broadlink device IDs
     "0x2712": "RM Pro+",
     "0x2737": "RM Mini",
 }
@@ -24,10 +25,15 @@ class Plugin(indigo.PluginBase):
         indigo.PluginBase.__init__(self, pid, name, version, prefs)
         self.debug = True
 
-    def _discover_device(self, values, type_id, did):
+    @staticmethod
+    def _discover_device(values, type_id, did):
         """ Devices.xml Callback Method to discover a Broadlink device. """
-        devices = broadlink.discover(timeout=9)
         values["address"] = "- discovery failed -"
+        try:
+            devices = broadlink.discover(timeout=9)
+        except Exception as err:
+            indigo.server.log(u"Error Discovering Devices! {1}".format(err), isError=True)
+            return values
         dev = indigo.devices[did]
         if dev.pluginProps.get("logChanges", True):
             indigo.server.log(u"Discovering Devices.")
@@ -38,14 +44,16 @@ class Plugin(indigo.PluginBase):
             return values  # only grab the first one.
         return values
 
-    def _get_saved_commands_list(self, filter, values, type_id, did):
+    @staticmethod
+    def _get_saved_commands_list(dev_filter, values, type_id, did):
         """ Devices.xml Callback Method to return saved commands for this device. """
         dev = indigo.devices[did]
         if "commands" not in dev.pluginProps:
             return [("none", "- none -")]
         return json.loads(dev.pluginProps["commands"])
 
-    def _delete_saved_commands(self, values, type_id, did):
+    @staticmethod
+    def _delete_saved_commands(values, type_id, did):
         """ Devices.xml Callback Method to delete saved commands. """
         if values["savedCommandList"]:
             dev = indigo.devices[did]
@@ -58,16 +66,23 @@ class Plugin(indigo.PluginBase):
             values["commands"] = props["commands"]
         return values
 
-    def _learn_new_command(self, values, type_id, did):
+    @staticmethod
+    def _learn_new_command(values, type_id, did):
         """ Devices.xml Callback Method to learn a new command. """
+        dev = indigo.devices[did]
         # If an address was provided, use it, otherwise, get it from the props.
-        addr = values.get("address", indigo.devices[did].pluginProps.get("address", "127.0.0.1"))
-        model = values.get("model", indigo.devices[did].pluginProps.get("model", "0x2712"))
-        # Magic.
-        bl_device = broadlink.gendevice(int(model, 0), (addr, 80), "000000000000")
-        bl_device.auth()
-        bl_device.enter_learning()
+        addr = values.get("address", dev.pluginProps.get("address", "127.0.0.1"))
+        model = values.get("model", dev.pluginProps.get("model", "0x2712"))
         values['rawCommand'] = "- learn failed -"
+        try:
+            # Magic.
+            bl_device = broadlink.gendevice(int(model, 0), (addr, 80), "000000000000")
+            bl_device.auth()
+            bl_device.enter_learning()
+        except Exception as err:
+            indigo.server.log(u"{0}, Error connecting to {1} ({2}): {3}"
+                              .format(MODELS[model], dev.name, addr, err), isError=True)
+            return values
         timeout = 9
         data = None
         while data is None and timeout > 0:
@@ -78,17 +93,18 @@ class Plugin(indigo.PluginBase):
             values['rawCommand'] = ''.join(format(x, '02x') for x in bytearray(data))
         return values
 
-    def _reset_command_counter(self, action, dev):
+    @staticmethod
+    def _reset_command_counter(action, dev):
         """ Set the command count for a device back to zero. """
         addr = dev.pluginProps.get("address", action.props.get("address", ""))
-        model = dev.pluginProps.get("model", action.props.get("model", ""))
+        model = dev.pluginProps.get("model", action.props.get("model", "0x2712"))
         dev.updateStateOnServer("commandCounter", 0)
         if dev.pluginProps.get("logChanges", True):
             indigo.server.log(u"{0}, Reset Command Counter for {1} ({2})"
                               .format(MODELS[model], dev.name, addr))
-        return
 
-    def _save_new_command(self, values, type_id, did):
+    @staticmethod
+    def _save_new_command(values, type_id, did):
         """ Devices.xml Callback Method to add a new command. """
         if values["commandName"] and values["rawCommand"]:
             dev = indigo.devices[did]
@@ -105,14 +121,16 @@ class Plugin(indigo.PluginBase):
             values["commandName"], values["rawCommand"] = "", ""
         return values
 
-    def _send_command(self, action, dev):
+    @staticmethod
+    def _send_command(action, dev):
         """ Actions.xml Callback: Send a Command. """
         cmd = action.props.get("rawCommand", "")
         addr = dev.pluginProps.get("address", action.props.get("address", ""))
-        model = dev.pluginProps.get("model", action.props.get("model", ""))
-        cmd_name = cmd
+        model = dev.pluginProps.get("model", action.props.get("model", "0x2712"))
+        cmd_name, raw_cmd = cmd, ""
         if cmd == "" or addr == "":
             return
+        # Try to match the raw command to a stored name.
         for raw_cmd, _name in json.loads(dev.pluginProps.get("commands", "[]")):
             if raw_cmd == cmd:
                 cmd_name = _name
@@ -120,10 +138,15 @@ class Plugin(indigo.PluginBase):
         if dev.pluginProps.get("logChanges", True):
             indigo.server.log(u"{0}, Sending Command to: {1} ({2}): {3}"
                               .format(MODELS[model], dev.name, addr, cmd_name))
-        bl_device = broadlink.gendevice(int(model, 0), (addr, 80), "000000000000")
-        bl_device.auth()
-        data = bytearray.fromhex(''.join(cmd))
-        bl_device.send_data(data)
+        try:
+            bl_device = broadlink.gendevice(int(model, 0), (addr, 80), "000000000000")
+            bl_device.auth()
+            data = bytearray.fromhex(''.join(cmd))
+            bl_device.send_data(data)
+        except Exception as err:
+            indigo.server.log(u"{0}, Error connecting to {1} ({2}): {3}"
+                              .format(MODELS[model], dev.name, addr, err), isError=True)
+            return
         dev.updateStateOnServer("commandCounter", dev.states.get("commandCounter", 0) + 1)
         if cmd_name == raw_cmd:
             dev.updateStateOnServer("lastRawCommand", raw_cmd)
